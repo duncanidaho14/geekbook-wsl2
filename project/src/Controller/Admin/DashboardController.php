fb41e2a6b174434e35b1c2f040bfe77cc73599ee
duncanidaho14 <elhadibeddarem@gmail.com>
2023-06-02 17:55:42 +0200

    dashboardcontroller

--- project/src/Controller/Admin/DashboardController.php
+++ project/src/Controller/Admin/DashboardController.php
@@ -31,8 +31,8 @@ class DashboardController extends AbstractDashboardController
 
         // Option 1. You can make your dashboard redirect to some common page of your backend
         //
-        // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
-        // return $this->redirect($adminUrlGenerator->setController(BookCrudController::class)->generateUrl());
+        $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+        return $this->redirect($adminUrlGenerator->setController(BookCrudController::class)->generateUrl());
 
         // Option 2. You can make your dashboard redirect to different pages depending on the user
         //
@@ -43,7 +43,7 @@ class DashboardController extends AbstractDashboardController
         // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
         // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
         //
-        return $this->render('some/path/my-dashboard.html.twig');
+        //return $this->render('admin/index.html.twig');
     }
 
     public function configureDashboard(): Dashboard
