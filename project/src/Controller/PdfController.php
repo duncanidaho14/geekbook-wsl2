5da5782d17157fa7d03a904ff0ed87d481005a14
duncanidaho14 <elhadibeddarem@gmail.com>
2023-06-14 19:44:23 +0200

    discard file add knp_snappy yaml & pdf Controller

---
+++ project/src/Controller/PdfController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Knp\Snappy\Pdf;
+use Knp\Bundle\SnappyBundle\Snappy\Response\PdfResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PdfController extends AbstractController
+{
+    #[Route("/account/order/pdf", name:"app_account_pdf_order")]
+    public function pdfAction(Pdf $knpSnappyPdf)
+    {
+        $pageUrl = $this->generateUrl('app_success_payment', array(), true);
+
+        return new PdfResponse(
+            $knpSnappyPdf->getOutput($pageUrl),
+            'file.pdf'
+        );
+    }
+}
