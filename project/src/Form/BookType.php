c1d1e384081814af9e11a72af6b6124fb99da4cc
duncanidaho14 <elhadibeddarem@gmail.com>
2023-07-29 23:44:46 +0200

    github action

--- project/src/Form/BookType.php
+++ project/src/Form/BookType.php
@@ -16,25 +16,99 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class BookType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title', TextType::class)
-            ->add('introduction', TextType::class)
+            ->add('title', TextType::class, [
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Le mot de passe ne peut être vide',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Votre titre doit comporter au moins {{ limit }} caractères',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096
+                    ])
+                ]
+            ])
+            ->add('introduction', TextType::class, [
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Le mot de passe ne peut être vide',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Votre introduction doit comporter au moins {{ limit }} caractères',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096
+                    ])
+                ]
+            ])
             ->add('description', TextareaType::class)
             ->add('publishedAt', DateTimeType::class, [
                 'input' => 'datetime_immutable',
             ])
             ->add('price', MoneyType::class)
-            ->add('langue', TextType::class)
+            ->add('langue', TextType::class, [
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Le mot de passe ne peut être vide',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Votre choix du langage doit comporter au moins {{ limit }} caractères',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096
+                    ])
+                ]
+            ])
             ->add('nbPages', NumberType::class)
-            ->add('dimension', TextType::class)
-            ->add('isbn', TextType::class)
-            ->add('editor', TextType::class)
+            ->add('dimension', TextType::class, [
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Le mot de passe ne peut être vide',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Votre dimension doit comporter au moins {{ limit }} caractères',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096
+                    ])
+                ]
+            ])
+            ->add('isbn', TextType::class, [
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Le mot de passe ne peut être vide',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Votre isbn doit comporter au moins {{ limit }} caractères',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096
+                    ])
+                ]
+            ])
+            ->add('editor', TextType::class, [
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Le mot de passe ne peut être vide',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Votre editeur doit comporter au moins {{ limit }} caractères',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096
+                    ])
+                ]
+            ])
             ->add('isInStock', CheckboxType::class)
             ->add('categories', EntityType::class, [
                 'class' => Category::class,
