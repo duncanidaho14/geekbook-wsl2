c0cf99b4871ae43b7c3370128dfc2c067806787d
duncanidaho14 <elhadibeddarem@gmail.com>
2024-04-01 17:40:54 +0200

    build hidora

--- docker/docker.sh
+++ docker/docker.sh
@@ -1,3 +1,7 @@
+composer install
+npm install
+php bin/console cache:clear
+php bin/console cache:warmup
 php bin/console doctrine:database:create
 php bin/console doctrine:migration:migrate
 exec apache2-foreground
