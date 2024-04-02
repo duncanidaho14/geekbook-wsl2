60eddd34eca790fc39260728a213a332ddfbe193
duncanidaho14 <elhadibeddarem@gmail.com>
2024-04-02 14:39:59 +0200

    docker.sh

--- docker/docker.sh
+++ docker/docker.sh
@@ -1,7 +1,9 @@
+cd project
 composer install
 npm install
 php bin/console cache:clear
 php bin/console cache:warmup
 php bin/console doctrine:database:create
 php bin/console doctrine:migration:migrate
+cd ..
 exec apache2-foreground
